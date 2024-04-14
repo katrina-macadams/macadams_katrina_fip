f62c308521a2def48bcb3c0be357161353104635
katrina-macadams <katrinamacadams@gmail.com>
2024-04-14 01:47:43 -0400

    Updated the HTML and CSS for all pages
    Added full html and css for all pages as well as some content

---
+++ js/main.js
@@ -0,0 +1,59 @@
+// VARIABLES
+
+(() => {
+    const slider = document.querySelector('.img_cont');
+    const prevBtn = document.querySelector('.left_arrow');
+    const nextBtn = document.querySelector ('.right_arrow'); 
+    let slideWidth = slider.clientWidth;
+    let currentIndex = 0;
+
+
+
+// FUNCTIONS
+
+function prevSlide () {
+    currentIndex--;
+    
+    if (currentIndex < 0) {
+        currentIndex = slider.children.length - 1; 
+    }
+}
+
+
+function nextSlide() {
+    currentIndex++;
+
+    if (currentIndex >= slider.children.length) {
+        currentIndex = 0; 
+    }
+}
+
+function updateSlideWidth () {
+    slideWidth = slider.clientWidth; 
+    showSlide(currentIndex); 
+}
+
+function showSlide (index) {
+    console.log(index);
+    const newTransformValue = -index * slideWidth;
+
+    slider.style.transform = `translateX(${newTransformValue}px)`;
+    
+}
+
+// EVENT LISTENERS
+
+prevBtn.addEventListener('click', prevSlide);
+nextBtn.addEventListener('click', nextSlide);
+window.addEventListener('click', updateSlideWidth);
+})();
+
+window.addEventListener('resize', function() {
+    // Calculate new height for the top section
+    var windowHeight = window.innerHeight;
+    document.querySelector('.top-section').style.height = windowHeight + 'px';
+});
+
+
+
+
